c3c8bde9227f5fdee440d0471aeed5926fb5672b
hetp4401 <patelhet32@gmail.com>
2021-06-21 16:19:59 -0400

    shows

--- imdb.js
+++ imdb.js
@@ -1,10 +1,20 @@
 const nameToImdb = require("name-to-imdb");
 
+const cacheManager = require("cache-manager");
+const imdbCache = cacheManager.caching({
+  store: "memory",
+  ttl: 1 * 60 * 60 * 6,
+});
+
 function getImdb(title) {
+  return imdbCache.wrap(title, () => fetchImdb(title));
+}
+
+function fetchImdb(title) {
   return new Promise((resolve, reject) => {
     nameToImdb(title.replace(/[^a-z0-9]/gi, ""), (err, res, inf) => {
       if (err || !inf.meta) {
-        return reject();
+        return reject(inf);
       }
       const { id } = inf.meta;
       return resolve({

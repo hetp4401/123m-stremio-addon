13ca6a3f049f2043f39a64487d95cd265756644c
hetp4401 <patelhet32@gmail.com>
2021-06-24 12:47:21 -0400

    test

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ app.use(express.static("static", { maxAge: "1y" }));
 app.use((req, res, next) => server(req, res, next));
 app.listen(process.env.PORT || 7000, () => {
   console.log("Add-on Repository URL: http://127.0.0.1:7000/manifest.json");
-
   startScraping();
   console.log("Started scrapers in the background");
 });

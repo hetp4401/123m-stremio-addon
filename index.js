180a82880b106336a786bf245cef0d85638ade15
hetp4401 <patelhet32@gmail.com>
2021-06-24 12:00:20 -0400

    fix

--- index.js
+++ index.js
@@ -1,37 +1,15 @@
-// const express = require("express");
-// const server = require("./server");
-
-// const { startScraping } = require("./scrapers/background/schedular");
-
-// const app = express();
-
-// app.use(express.static("static", { maxAge: "1y" }));
-// app.use((req, res, next) => server(req, res, next));
-// app.listen(process.env.PORT || 7000, () => {
-//   console.log("Add-on Repository URL: http://127.0.0.1:7000/manifest.json");
-
-//   startScraping();
-//   console.log("Started scrapers in the background");
-// });
-
 const express = require("express");
+const server = require("./server");
+
+const { startScraping } = require("./scrapers/background/schedular");
+
 const app = express();
-const cors = require("cors");
 
-const HOST = "https://123m-stremio-addon.billybishop.repl.co";
-
-app.use(cors());
-
-app.get("/manifest.json", (req, res) => {
-  res.writeHead(302, { Location: HOST + req.url });
-  res.end();
-});
-
-app.get("/:resource/:type/:id/:extra?.json", (req, res) => {
-  res.writeHead(302, { Location: HOST + req.url });
-  res.end();
-});
-
+app.use(express.static("static", { maxAge: "1y" }));
+app.use((req, res, next) => server(req, res, next));
 app.listen(process.env.PORT || 7000, () => {
-  console.log("Server is up");
+  console.log("Add-on Repository URL: http://127.0.0.1:7000/manifest.json");
+
+  startScraping();
+  console.log("Started scrapers in the background");
 });

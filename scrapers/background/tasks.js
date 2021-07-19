f21b8a8becf31441be4dbc29c9dc5cafd4229ed3
hetp4401 <patelhet32@gmail.com>
2021-07-18 23:21:04 -0400

    temp

--- scrapers/background/tasks.js
+++ scrapers/background/tasks.js
@@ -2,14 +2,14 @@ const lmMovies = require("./lookmovie/movies").getMovies;
 const lmShows = require("./lookmovie/shows").getShows;
 
 module.exports = [
-  // {
-  //   name: "lookMovieMovies",
-  //   scraper: lmMovies,
-  //   cron: "54 * * * *",
-  // },
-  // {
-  //   name: "lookMovieShows",
-  //   scraper: lmShows,
-  //   cron: "56 * * * *",
-  // },
+  {
+    name: "lookMovieMovies",
+    scraper: lmMovies,
+    cron: "5 */6 * * *",
+  },
+  {
+    name: "lookMovieShows",
+    scraper: lmShows,
+    cron: "20 */6 * * *",
+  },
 ];

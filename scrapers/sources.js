6c21a09c372074f11c18e8fc539fe2fcf1e87c9b
hetp4401 <patelhet32@gmail.com>
2021-09-03 15:07:29 -0400

    updated sources

--- scrapers/sources.js
+++ scrapers/sources.js
@@ -5,19 +5,27 @@ const vidsrc = require("./external/vidsrc").getExternal;
 module.exports = [
   {
     name: "db",
-    prefix: "",
+    prefix: "lm",
     method: () => {},
     memoryttl: 1 * 60 * 15,
     remotettl: 99999999,
     useCache: true,
   },
   {
-    name: "dbgo.fun",
-    method: dbgo,
+    name: "db",
+    prefix: "tr",
+    method: () => {},
     memoryttl: 1 * 60 * 15,
-    remotettl: 1 * 60 * 60 * 4,
+    remotettl: 99999999,
     useCache: true,
   },
+  // {
+  //   name: "dbgo.fun",
+  //   method: dbgo,
+  //   memoryttl: 1 * 60 * 15,
+  //   remotettl: 1 * 60 * 60 * 4,
+  //   useCache: true,
+  // },
   // {
   //   name: "2embed.ru",
   //   method: embed,
